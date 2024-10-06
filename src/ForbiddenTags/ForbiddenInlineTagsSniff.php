35d012b36a6c03bd30e4dcdcd7af61566447d71f
James Hunt <10615884+thetwopct@users.noreply.github.com>
2024-10-06 19:12:57 +0700

    Initial code
    Signed-off-by: James Hunt <10615884+thetwopct@users.noreply.github.com>

---
+++ src/ForbiddenTags/ForbiddenInlineTagsSniff.php
@@ -0,0 +1,23 @@
+<?php
+namespace WP_Org_Submission_Rules\ForbiddenTags;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class ForbiddenInlineTagsSniff implements Sniff
+{
+    public function register()
+    {
+        return [T_INLINE_HTML];
+    }
+
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $content = strtolower($tokens[$stackPtr]['content']);
+
+        if (strpos($content, '<script') !== false || strpos($content, '<style') !== false) {
+            $phpcsFile->addError('Inline <script> or <style> tags are forbidden.', $stackPtr, 'ForbiddenTags');
+        }
+    }
+}
